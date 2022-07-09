5feb8f68e0be38138d489ec6a565b7e405862a1b
macandcheese808 <macandcheese808@icloud.com>
2022-07-09 16:00:00 +0200

    uploaded via bash

---
+++ password-generator.py
@@ -0,0 +1,14 @@
+import random
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "!$%&@*-+?"
+
+string = lower + upper + numbers + symbols
+length = 16
+password = "".join(random.sample(string, length))
+
+print("Your Random Password Is:")
+print()
+print(password)
